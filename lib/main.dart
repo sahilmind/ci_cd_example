46c7f02252d1837727c25f611926984fbe67f793
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2023-06-21 14:56:29 +0530

    refactor(#workflow): Updated workflow file.

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
             ),
             Text(
